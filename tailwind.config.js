76342b14d9c7932c646af4ab589dab5e96c62f86
Dmytro Savenko <soldivoldi@gmail.com>
2024-11-18 23:42:24 +0100

    add project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      main: ["Open Sans"],
+    },
+    rotate: {
+      135: "135deg",
+    },
+    plugins: [],
+  },
+};
