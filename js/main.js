2200fb4ae9cbf288b92454990d9f6cc226d038c6
Dmytro Savenko <soldivoldi@gmail.com>
2024-11-19 03:57:41 +0100

    add window to order and add hover to a tegs

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ const mobileMenu = document.getElementById("mobile-menu");
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+const openModalButton = document.getElementById("openModal");
+const closeModalButton = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+const body = document.body;
+
+openModalButton.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  body.classList.add("overflow-hidden");
+});
+
+closeModalButton.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  body.classList.remove("overflow-hidden");
+});
